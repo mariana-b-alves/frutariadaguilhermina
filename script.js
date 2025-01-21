e82c5650b1f803f835d113eefd5c31e6ad9fc678
Mariana Alves <m.bourdain.alves>
2025-01-21 11:00:56 +0000

    Fixed responsive on the main page

--- script.js
+++ script.js
@@ -13,24 +13,3 @@ function showSlides() {
     slides[slideIndex-1].classList.add('active');
     setTimeout(showSlides, 7000); // Muda a imagem a cada 5 segundos
 }
-
-/*Abrir e fechar botão do menu mobile - popup (menu) aparece e desaparece*/
-var openBtn = document.querySelector(".open-btn");
-var closeBtn = document.querySelector(".close-btn");
-var popup = document.querySelectorAll(".popup");
-
-//Quando o botão é clicado, o menu aparece
-
-openBtn.addEventListener('click', function (){
-    for(let x = 0; x < popup.length; x++ ){
-        popup[x].classList.add('visible');
-    }
-})
-
-///Quando o botão é clicado, o menu desaparece
-
-closeBtn.addEventListener('click', function (){
-    for(let x = 0; x < popup.length; x++ ){
-        popup[x].classList.remove('visible');
-    }
-})
